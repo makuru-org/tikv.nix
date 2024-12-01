866aa77866d7b54b0a490e1c749f4706388fb07b
Makuru <Makuru@Makuru.org>
2024-12-01 04:05:13 +0100

    Updated dependencies.

--- flake.nix
+++ flake.nix
@@ -2,90 +2,101 @@
   inputs = {
     fenix = {
       url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "git+file:///home/framework-16/Documents/nixpkgs";
   };
 
-  outputs = { self, fenix, flake-utils, nixpkgs }:
+  outputs =
+    {
+      fenix,
+      flake-utils,
+      nixpkgs,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (system: {
-      packages.tikv =
-        let 
-          #toolchain = fenix.packages.${system}.fromManifest (nixpkgs.lib.importTOML ./Cargo.toml);
-          toolchain = fenix.packages.${system}.fromToolchainFile { file = ./rust-toolchain.toml; sha256 ="sha256-4HfRQx49hyuJ8IjBWSty3OXCLOmeaZF5qZAXW6QiQNI=";};
+     packages.tikv =
+        let
+          toolchain = fenix.packages.${system}.fromToolchainFile {
+            file = ./rust-toolchain.toml;
+            sha256 = "sha256-4HfRQx49hyuJ8IjBWSty3OXCLOmeaZF5qZAXW6QiQNI=";
+          };
           pkgs = nixpkgs.legacyPackages.${system};
         in
 
         (pkgs.makeRustPlatform {
           cargo = toolchain;
           rustc = toolchain;
-        }).buildRustPackage rec {
-          pname = "tikv";
-          version = "8.1.0";
+        }).buildRustPackage
+          rec {
+            pname = "tikv";
+            version = "8.4.0";
 
-          src = pkgs.fetchFromGitHub {
-            owner = "tikv";
-            repo = pname;
-            rev = "v${version}";
-            sha256 = "sha256-oAyD2+LMHINZN8Kx87KxKLyOwf0EYnLMoJsoRZvmvBY=";
-          };
+            src = pkgs.fetchFromGitHub {
+              owner = pname;
+              repo = pname;
+              rev = "v${version}";
+              sha256 = "sha256-CKt1Jh3UByB9hXc9WkXhevfKx+DNo1VUSV9Vvt2bJd4=";
+            };
 
-          lz4 = fetchTarball {
-            url = "https://github.com/lz4/lz4/releases/download/v1.9.4/lz4-1.9.4.tar.gz";
-            sha256 = "sha256:14l616ja261x5cdb2rh6y00qpzcqq8m8a80mqjl6zb7ggc7h48v2";
-          };
+            # ROCKSDB_DIR = "${pkgs.rocksdb_6_23}";
 
-          nativeBuildInputs = [
-            pkgs.pkg-config
-            pkgs.cmake
-            pkgs.rustPlatform.bindgenHook
-            pkgs.protobuf
-            pkgs.snappy
-            pkgs.zlib
-            pkgs.lzlib
-            lz4
-         ];
+            nativeBuildInputs = [
+              pkgs.pkg-config
+              pkgs.cmake
+              pkgs.rustPlatform.bindgenHook
+              pkgs.protobuf
+              #pkgs.snappy
+              #pkgs.zlib
+              #pkgs.lzlib
+              pkgs.git
+            ];
 
-          buildInputs = [
-            pkgs.pkg-config
-            pkgs.gnumake
-            pkgs.openssl
-            pkgs.snappy
-            pkgs.go
-            pkgs.gawk
-            pkgs.libllvm
-            pkgs.llvmPackages.clangUseLLVM
-            pkgs.libgcc
-            pkgs.binutils
-          ];
+            buildInputs = [
+              pkgs.pkg-config
+              pkgs.gnumake
+              pkgs.openssl
+              pkgs.snappy
+              pkgs.go
+              pkgs.gawk
+              pkgs.libllvm
+              pkgs.llvmPackages.clangUseLLVM
+              pkgs.gcc9
+              pkgs.binutils
+              pkgs.lz4
+              pkgs.zstd
+              pkgs.zlib
+              pkgs.bzip2
+            ];
 
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-            outputHashes = {
-              "azure_core-0.12.0" = "sha256-1Y+4puMrtrKoKWxrNRl8MzBhX298NzT6V4/SVisNYbE=";
-              "cmake-0.1.48" = "sha256-yFHJUzaiwvAyah6wahfeY7qNvtiN0nmsVSWr/Sc/tWk=";
-              "encoding_rs-0.8.29" = "sha256-YbV9szN2nIxrEx0MKMFzJet8pKtNHv0ryghmvGfvUbE=";
-              "fs2-0.4.3" = "sha256-ZaCu23Vzkpz+2AwL5n4lrHN+Pn4xFYC2awtaKil/Uxo=";
-              "kvproto-0.0.2" = "sha256-g01XxpUMy4RwSiKjxpaYNLGr22Vc5aPYsz4wG7AqT0Q=";
-              "librocksdb_sys-0.1.0" = "sha256-Tl+62DLy9U6PvatiCeVG7IV0fyOu+e8mGpZRrJRCxpA=";
-              "procinfo-0.4.2" = "sha256-r0MgE8cNAf5LDgRQAtVDXBgck5eK67jAiN6lOZVsJyo=";
-              "protobuf-2.8.0" = "sha256-bg8osI/zg9S7EckscdX4An+uy9yTOstiZypDu/85JMk=";
-              "raft-0.7.0" = "sha256-77wPQoEG8Q0L8kPiWzB7NQLqx9T8o1U9vEMc0Prn9ho=";
-              "raft-engine-0.4.2" = "sha256-AnIJJ87PA41QKpaxLkqfNxazjTo/Md2hO9aYG0JNrsQ=";
-              "rusoto_core-0.46.0" = "sha256-JzDw+oZYSypurHvR31lD3Hjq2kfiGsgQ/14Z59IEMgg=";
-              "skiplist-rs-0.1.0" = "sha256-3dVMAofINOObEYLtD0SuVfwCi0dmeslEq9ZcoGKd4wU=";
-              "slog-global-0.1.0" = "sha256-1EoGVI0wHoSKG4yO9SjwLXYYIawYoTaLz15rWlmK7Kg=";
-              "snappy-sys-0.1.0" = "sha256-7EKiW6RZuBOeX0Yp3Arf+ll14IwH+1QiR5vDNJomc0A=";
-              "sysinfo-0.26.9" = "sha256-wsxUdNCX1nJwaS4kedhET2UBr9wY8Q4EB06I0w3JEIY=";
-              "tame-oauth-0.9.6" = "sha256-dE5AWImaeWXP2SeGLryhGy82IONSPXAUuOXnLqL80x8=";
-              "tipb-0.0.1" = "sha256-65ptYmqWaY8X3/3NUd8bwnzu+9KzXGDTErwJhLg6efo=";
-              "tokio-executor-0.1.9" = "sha256-Ku9MnT7uhyWhXOc5vpcoGmxyTNN7EL5GT6c95S3r/4U=";
-              "tracing-active-tree-0.1.0" = "sha256-EXKbt7nf0nlU89T80R5c4Iezji5GOWylMtCHmUeB86o=";
-              "yatp-0.0.1" = "sha256-AQ7CzDT43tvbLdFIoEuTsK3KSubZFAQr0XrD81uwQbw=";
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              outputHashes = {
+                "azure_core-0.12.0" = "sha256-1Y+4puMrtrKoKWxrNRl8MzBhX298NzT6V4/SVisNYbE=";
+                "cmake-0.1.48" = "sha256-yFHJUzaiwvAyah6wahfeY7qNvtiN0nmsVSWr/Sc/tWk=";
+                "encoding_rs-0.8.29" = "sha256-YbV9szN2nIxrEx0MKMFzJet8pKtNHv0ryghmvGfvUbE=";
+                "fs2-0.4.3" = "sha256-ZaCu23Vzkpz+2AwL5n4lrHN+Pn4xFYC2awtaKil/Uxo=";
+                "kvproto-0.0.2" = "sha256-IQNJGkatLCiozTaf/+1fs/3lVQk6JViEuGjMh+wC6O8=";
+                "librocksdb_sys-0.1.0" = "sha256-UcLCXLC3pM2s8vc7bi9IrtdLdjkv3xSPI7EjQvxrMYQ=";
+                "procinfo-0.4.2" = "sha256-r0MgE8cNAf5LDgRQAtVDXBgck5eK67jAiN6lOZVsJyo=";
+                "protobuf-2.8.0" = "sha256-Kzs5Gt06cNJROPXaQX8AIzFhoBYTs7ubEOGkccwhPJw=";
+                "raft-0.7.0" = "sha256-77wPQoEG8Q0L8kPiWzB7NQLqx9T8o1U9vEMc0Prn9ho=";
+                "raft-engine-0.4.2" = "sha256-AnIJJ87PA41QKpaxLkqfNxazjTo/Md2hO9aYG0JNrsQ=";
+                "raft-engine-ctl-0.4.2" = "sha256-AnIJJ87PA41QKpaxLkqfNxazjTo/Md2hO9aYG0JNrsQ=";
+                "rusoto_core-0.46.0" = "sha256-lnMcSpytw743FYfR12b6NUTrLwR8NCP8qrwELcjsS4g=";
+                # "skiplist-rs-0.1.0" = "sha256-3dVMAofINOObEYLtD0SuVfwCi0dmeslEq9ZcoGKd4wU=";
+                "slog-global-0.1.0" = "sha256-1EoGVI0wHoSKG4yO9SjwLXYYIawYoTaLz15rWlmK7Kg=";
+                "snappy-sys-0.1.0" = "sha256-7EKiW6RZuBOeX0Yp3Arf+ll14IwH+1QiR5vDNJomc0A=";
+                "sysinfo-0.26.9" = "sha256-wsxUdNCX1nJwaS4kedhET2UBr9wY8Q4EB06I0w3JEIY=";
+                "tame-oauth-0.9.6" = "sha256-dE5AWImaeWXP2SeGLryhGy82IONSPXAUuOXnLqL80x8=";
+                "tipb-0.0.1" = "sha256-WpJSHacVlLVYfCS+3ODdTTAc4nxbezLcawFvRdPseVI=";
+                "tokio-executor-0.1.9" = "sha256-Ku9MnT7uhyWhXOc5vpcoGmxyTNN7EL5GT6c95S3r/4U=";
+                "tracing-active-tree-0.1.0" = "sha256-EXKbt7nf0nlU89T80R5c4Iezji5GOWylMtCHmUeB86o=";
+                "yatp-0.0.1" = "sha256-AQ7CzDT43tvbLdFIoEuTsK3KSubZFAQr0XrD81uwQbw=";
+              };
             };
           };
-        };
-    }
-  );
+    });
 }
